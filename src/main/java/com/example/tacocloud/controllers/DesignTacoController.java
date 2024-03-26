43b72d8ea1c4a17dd6489720af30ba42bd1240c0
yakudzza <andrewlutov1@gmail.com>
2024-03-26 19:48:57 +0300

    new controllers

--- src/main/java/com/example/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/controllers/DesignTacoController.java
@@ -1,2 +1,20 @@
-package com.example.tacocloud.controllers;public class DesignTacoController {
+package com.example.tacocloud.controllers;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("tacoOrder")
+public class DesignTacoController {
+
+    public void addIngridientsToModel(){
+
+
+    }
+
 }
